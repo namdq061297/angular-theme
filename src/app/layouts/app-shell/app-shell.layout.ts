60829ec2ed004f4db74c6413c35f3a66e32f8310
namdq <namdq97@gmail.com>
2026-05-20 13:47:11 +0700

    feat: add icon and image components with SVG support and update layout

--- src/app/layouts/app-shell/app-shell.layout.ts
+++ src/app/layouts/app-shell/app-shell.layout.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthStateService } from '../../core/services/auth-state.service';
+import { IconComponent } from '../../shared/components/icon/icon.component';
 
 @Component({
   selector: 'app-shell-layout',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, IconComponent],
   templateUrl: './app-shell.layout.html',
   styleUrl: './app-shell.layout.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
